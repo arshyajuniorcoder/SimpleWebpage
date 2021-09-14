3503644b88de4a370946444789e9c4c54a5f97cc
arshyajuniorcoder <89856131+arshyajuniorcoder@users.noreply.github.com>
2021-09-14 23:39:58 +0430

    Add files via upload

---
+++ simple-page/changeImage.js
@@ -0,0 +1,38 @@
+function changeFirstImage() {
+    if (
+        (document.getElementById("first-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094499.jpg?x-oss-process=image/resize,h_1600/quality,q_80")
+    ) {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094499.jpg?x-oss-process=image/resize,h_1600/quality,q_80";
+    } else {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094486.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90";
+    }
+}
+
+function changeSecondImage() {
+    if (
+        (document.getElementById("second-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094497.jpg?x-oss-process=image/resize,h_1600/quality,q_80")
+    ) {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094497.jpg?x-oss-process=image/resize,h_1600/quality,q_80";
+    } else {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094486.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90";
+    }
+}
+
+function changeThirdImage() {
+    if (
+        (document.getElementById("third-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094492.jpg?x-oss-process=image/resize,h_1600/quality,q_80")
+    ) {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094492.jpg?x-oss-process=image/resize,h_1600/quality,q_80";
+    } else {
+        document.getElementById("product-main-image").src =
+            "https://dkstatics-public.digikala.com/digikala-products/3094486.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90";
+    }
+}
